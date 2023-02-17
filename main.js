8b0f2705edfe635355b20603af3637900abfcc46
josue josuerushanika <100031798+josuerushanika@users.noreply.github.com>
2023-02-16 20:14:35 -0800

    call DOM

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-fetch('http://example.com/movies.json')
-.then((response) => response.json())
-.then((data) => console.log(data));
+const API_URL = "https://api.themoviedb.org/3//discover/movie?sort_by=popularity.desc&api_key=0f2c201def0837236210a419bd798f2c";
+
+const IMAGE_PATH = "https://image.tmdb.org/t/p/w1280";
+
+const SEARCH_URL = "https://api.themoviedb.org/3//search/movie?sort_by=popularity.desc&api_key=0f2c201def0837236210a419bd798f2c";
+
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+const main = document.getElementById('main')
+
+
